94eb19aa58383930e504bbd0784fb4afcba69322
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-02-16 16:25:46 -0600

    Default Profile

--- section-3-dependency-injection/src/main/java/guru/springframework/injection/dependency/services/I18nSpanishService.java
+++ section-3-dependency-injection/src/main/java/guru/springframework/injection/dependency/services/I18nSpanishService.java
@@ -3,7 +3,7 @@ package guru.springframework.injection.dependency.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishService implements GreetingService {
 
