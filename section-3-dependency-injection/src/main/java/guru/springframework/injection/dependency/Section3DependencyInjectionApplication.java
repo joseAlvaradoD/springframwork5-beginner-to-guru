754f15d848cdc52e48f6e9c376c9b85fd2bef045
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-02-16 12:46:50 -0600

    Basics of Dependency Injection

--- section-3-dependency-injection/src/main/java/guru/springframework/injection/dependency/Section3DependencyInjectionApplication.java
+++ section-3-dependency-injection/src/main/java/guru/springframework/injection/dependency/Section3DependencyInjectionApplication.java
@@ -1,13 +1,20 @@
 package guru.springframework.injection.dependency;
 
+import guru.springframework.injection.dependency.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Section3DependencyInjectionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Section3DependencyInjectionApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(Section3DependencyInjectionApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greetings = myController.sayHello();
+
+		System.out.println(greetings);
 	}
 
 }
