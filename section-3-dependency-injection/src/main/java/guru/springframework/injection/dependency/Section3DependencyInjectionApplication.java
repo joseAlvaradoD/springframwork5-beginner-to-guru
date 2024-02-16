b3fc7907f5607f68b3a54bab3ba2180a25880c85
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-02-16 12:16:11 -0600

    Created spring project for section 3 Dependency Injection

---
+++ section-3-dependency-injection/src/main/java/guru/springframework/injection/dependency/Section3DependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.injection.dependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section3DependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section3DependencyInjectionApplication.class, args);
+	}
+
+}
