6ef295d0eb6ce346de7374df9e3dc7b618fa7b46
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-02-23 09:00:32 -0600

    creating jokes app and implementing jokeService

---
+++ section-4-jokes-app/src/main/java/guru/springframework/jokes/services/JokeService.java
@@ -0,0 +1,5 @@
+package guru.springframework.jokes.services;
+
+public interface JokeService {
+    String getJoke();
+}
---
+++ section-4-jokes-app/src/main/java/guru/springframework/jokes/services/JokeServiceImpl.java
@@ -0,0 +1,20 @@
+package guru.springframework.jokes.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService{
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl(){
+        chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke(){
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+
+}
