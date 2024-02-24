a0b93d9d73ca7a8998f21288e59d36768f0e96b9
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-02-24 09:35:50 -0600

    Using XML Configuration

--- section-3-dependency-injection/src/main/java/guru/springframework/di/config/GreetingServiceConfig.java
+++ section-3-dependency-injection/src/main/java/guru/springframework/di/config/GreetingServiceConfig.java
@@ -6,11 +6,9 @@ import guru.springframework.di.services.*;
 import guru.springframework.pets.services.DogPetService;
 import guru.springframework.pets.services.PetService;
 import guru.springframework.pets.services.PetServiceFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 
+@ImportResource("classpath:sfg.config.xml")
 @Configuration
 public class GreetingServiceConfig {
 
@@ -52,11 +50,6 @@ public class GreetingServiceConfig {
     @Bean
     PrimaryGreetingService primaryGreetingService(){
         return new PrimaryGreetingService();
-    }
-
-    @Bean
-    ConstructorInjectedGreetingService constructorInjectedGreetingService(){
-        return new ConstructorInjectedGreetingService();
     }
 
     @Bean
