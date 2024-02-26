92aeb7c88afc6cb95cf3d8acad5e9d62e9d794ab
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-02-26 13:26:57 -0600

    adding recipe project

---
+++ section-7-recipe-project/src/main/java/jjad/springframework/recipe/Section7RecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package jjad.springframework.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section7RecipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section7RecipeProjectApplication.class, args);
+	}
+
+}
