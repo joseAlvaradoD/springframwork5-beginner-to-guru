8c1799272f0dce054ad7acfebea81453517c2ef4
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-02-29 13:01:55 -0600

    adding list of recipies

--- section-7-recipe-project/src/main/java/jjad/springframework/recipe/controllers/IndexController.java
+++ section-7-recipe-project/src/main/java/jjad/springframework/recipe/controllers/IndexController.java
@@ -3,8 +3,10 @@ package jjad.springframework.recipe.controllers;
 import jjad.springframework.recipe.model.Category;
 import jjad.springframework.recipe.model.UnitOfMeasure;
 import jjad.springframework.recipe.repositories.CategoryRepository;
+import jjad.springframework.recipe.repositories.RecipeRepository;
 import jjad.springframework.recipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -12,21 +14,15 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private RecipeRepository recipeRepository;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeRepository recipeRepository){
+        this.recipeRepository = recipeRepository;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String index(){
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("Mexican");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-
-        System.out.println("Cat Id is: " + categoryOptional.get().getId());
-        System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
+    public String index(Model model){
+        model.addAttribute("recipes", recipeRepository.findAll());
 
         return "index";
     }
--- section-7-recipe-project/src/main/java/jjad/springframework/recipe/model/Recipe.java
+++ section-7-recipe-project/src/main/java/jjad/springframework/recipe/model/Recipe.java
@@ -2,6 +2,7 @@ package jjad.springframework.recipe.model;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,6 +17,7 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+    @Lob
     private String directions;
     @Lob
     private Byte[] image;
@@ -24,12 +26,12 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
     @ManyToMany
     @JoinTable(name = "recipe_category",
             joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
 
     public Long getId() {
         return id;
