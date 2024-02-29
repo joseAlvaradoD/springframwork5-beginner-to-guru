cd45efa095f23e35539aab2e9c3de6ed6317a256
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-02-29 11:53:03 -0600

    adding spring jpa query

--- section-7-recipe-project/src/main/java/jjad/springframework/recipe/controllers/IndexController.java
+++ section-7-recipe-project/src/main/java/jjad/springframework/recipe/controllers/IndexController.java
@@ -1,13 +1,33 @@
 package jjad.springframework.recipe.controllers;
 
+import jjad.springframework.recipe.model.Category;
+import jjad.springframework.recipe.model.UnitOfMeasure;
+import jjad.springframework.recipe.repositories.CategoryRepository;
+import jjad.springframework.recipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String index(){
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("Mexican");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is: " + categoryOptional.get().getId());
+        System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
