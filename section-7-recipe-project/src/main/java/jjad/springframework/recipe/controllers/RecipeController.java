9b483756aa7803411217496eee34fce0c95e3ad5
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-03-07 18:01:58 -0600

    Adding delete recipe functionality

--- section-7-recipe-project/src/main/java/jjad/springframework/recipe/controllers/RecipeController.java
+++ section-7-recipe-project/src/main/java/jjad/springframework/recipe/controllers/RecipeController.java
@@ -5,10 +5,7 @@ import jjad.springframework.recipe.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
@@ -21,23 +18,30 @@ public class RecipeController {
         this.recipeService = recipesService;
     }
 
-    @RequestMapping("/{id}/show")
+    @GetMapping("/{id}/show")
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
         return "recipe/show";
     }
 
-    @RequestMapping("/new")
+    @GetMapping("/new")
     public String newRecipe(Model model){
         model.addAttribute("recipe", new RecipeCommand());
 
         return "recipe/recipeform";
     }
 
-    @RequestMapping("/{id}/update")
+    @GetMapping("/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
         return  "recipe/recipeform";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteRecipe(@PathVariable String id){
+        log.debug("deleting by id: " + id);
+        recipeService.deleteById(Long.valueOf(id));
+        return  "redirect:/";
     }
 
     @PostMapping("")
