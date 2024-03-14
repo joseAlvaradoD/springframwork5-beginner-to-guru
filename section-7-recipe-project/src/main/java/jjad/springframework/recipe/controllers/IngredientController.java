9ec456813630a0097fd2320a02782e2349e5ee6b
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-03-14 12:15:21 -0600

    adding list of ingredients view

---
+++ section-7-recipe-project/src/main/java/jjad/springframework/recipe/controllers/IngredientController.java
@@ -0,0 +1,29 @@
+package jjad.springframework.recipe.controllers;
+
+import jjad.springframework.recipe.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Slf4j
+@Controller
+public class IngredientController {
+
+    private RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("/recipe/{recipeId}/ingredients")
+    public String listIngredients(@PathVariable String recipeId, Model model){
+        log.debug("Getting ingredients list for recipe id: " + recipeId);
+
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+
+        return "recipe/ingredient/list";
+    }
+}
