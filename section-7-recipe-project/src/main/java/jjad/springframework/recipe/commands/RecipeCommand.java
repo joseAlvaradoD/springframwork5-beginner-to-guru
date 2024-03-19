bfada001246cf87ad9aeef7efd7f4f2c33362e4e
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-03-19 17:01:55 -0600

    adding validation for save or update recipe

--- section-7-recipe-project/src/main/java/jjad/springframework/recipe/commands/RecipeCommand.java
+++ section-7-recipe-project/src/main/java/jjad/springframework/recipe/commands/RecipeCommand.java
@@ -1,9 +1,14 @@
 package jjad.springframework.recipe.commands;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import jjad.springframework.recipe.model.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -16,12 +21,28 @@ import java.util.Set;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String description;
+
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
+
+    @Min(1)
+    @Max(100)
     private Integer servings;
     private String source;
+
+    @URL
     private String url;
+
+    @NotBlank
     private String directions;
     private Byte[] image;
     private Set<IngredientCommand> ingredients = new HashSet<>();
--- section-7-recipe-project/src/test/java/jjad/springframework/recipe/controllers/RecipeControllerTest.java
+++ section-7-recipe-project/src/test/java/jjad/springframework/recipe/controllers/RecipeControllerTest.java
@@ -93,7 +93,9 @@ class RecipeControllerTest {
         when(recipeService.saveRecipeCommand(any())).thenReturn(command);
 
         mockMvc.perform(post("/recipe")
-                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .param("description", "some description")
+                    .param("directions", "some directions")
                 )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
