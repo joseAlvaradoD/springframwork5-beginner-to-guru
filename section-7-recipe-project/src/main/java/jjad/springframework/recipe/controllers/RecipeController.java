96fa8d15639d1c84b4a16efe51d11e89d0169520
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-03-19 13:22:11 -0600

    adding ExceptionHanlder to Recipe controller

--- section-7-recipe-project/src/main/java/jjad/springframework/recipe/controllers/RecipeController.java
+++ section-7-recipe-project/src/main/java/jjad/springframework/recipe/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package jjad.springframework.recipe.controllers;
 
 import jjad.springframework.recipe.commands.RecipeCommand;
+import jjad.springframework.recipe.exception.NotFoundException;
 import jjad.springframework.recipe.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -49,6 +52,19 @@ public class RecipeController {
         RecipeCommand savedRecipe = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/"+savedRecipe.getId()+"/show";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 
 }
--- section-7-recipe-project/src/test/java/jjad/springframework/recipe/controllers/RecipeControllerTest.java
+++ section-7-recipe-project/src/test/java/jjad/springframework/recipe/controllers/RecipeControllerTest.java
@@ -61,7 +61,8 @@ class RecipeControllerTest {
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(get("/recipe/1/show"))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404error"));
     }
 
     @Test
